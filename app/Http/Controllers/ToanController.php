ff98868c000956c1189a2fcd70c86cd786173885
thinh180499 <thinh180499@gmail.com>
2023-06-22 23:23:52 +0700

    updata lua thua voi mu n

--- app/Http/Controllers/ToanController.php
+++ app/Http/Controllers/ToanController.php
@@ -10,12 +10,32 @@ class ToanController extends Controller
         return view('toan.luythuamunguyenduong');
     }
     public function tinhluythuamunguyenduong(){
-        if(isset($_POST['='])&&($_POST['='])){
+        //xét biến $a có phải là một số hữu hạn
+        if(is_numeric($_POST['a']) && is_finite($_POST['a'])){
             $a=$_POST['a'];
-            $b=$_POST['b'];
-            $ketqua=pow($a,$b);
-            return view('toan.luythuamunguyenduong',compact('ketqua','a','b'));
+             //xét biến $b có phải là một số hữu hạn
+            if($_POST['b'] && is_finite($_POST['b'])){
+                $b=$_POST['b'];
+                //tính kết quả
+                $ketqua=pow($a,$b);
+                //xét kết quả là số vô hạn
+                if(is_infinite($ketqua))
+                {
+                    $ketqua="kết quả vượt qua giới hạn tính";
+                    return view('toan.luythuamunguyenduong',compact('ketqua','a','b'));
+                }
+                else{
+                    return view('toan.luythuamunguyenduong',compact('ketqua','a','b'));
+                }
+            }
+            else{
+                $ketqua="nhập n";
+                return view('toan.luythuamunguyenduong',compact('ketqua','a'));
+            }
         }
-        return view('toan.luythuamunguyenduong');
+        else{
+            $ketqua="nhập a";
+            return view('toan.luythuamunguyenduong',compact('ketqua'));
+        }
     }
 }
