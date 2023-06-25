17075e7165f23aee8e76a590e3d7d70e4d481838
thinh180499 <thinh180499@gmail.com>
2023-06-25 13:55:39 +0700

    updata cuongdodongdien 2

--- app/Http/Controllers/VatlyController.php
+++ app/Http/Controllers/VatlyController.php
@@ -524,44 +524,30 @@ class VatlyController extends Controller
         return view('vatly.cuongdodongdien');
     }
     public function tinhcuongdodongdien(Request $request){
-            //xét biến $a có phải là một số lớn hơn 0 và hữu hạn
-        if(is_numeric($_POST['a']) && is_finite($_POST['a']) && $_POST['a']>0)
-            {
-                $a=$_POST['a'];
-                //xét biến $b có phải là một số lớn hơn 0 và hữu hạn
-                if($_POST['b'] && is_finite($_POST['b']) && $_POST['b']>0)
-                {
-                    $b=$_POST['b'];
-                    //tính kết quả
-                    (float)$ketqua=$a/$b;
-                    //xét kết quả khác 0
-                    if($ketqua)
-                    {
-                        //xét kết quả là số vô hạn
-                        if(is_infinite($ketqua))
-                        {
-                            $ketqua="kết quả vượt qua giới hạn tính";
-                            return view('vatly.cuongdodongdien',compact('ketqua','a','b'));
-                        }
-                        else{
-                            return view('vatly.cuongdodongdien',compact('ketqua','a','b'));
-                        }
-                    }
-                    else {
-                        static $ketqua=0;
-                        return view('vatly.cuongdodongdien',compact('ketqua','a','b'));
-                    }
-                }
-                else{
-                    $ketqua="nhập R với R khác 0";
-                    return view('vatly.cuongdodongdien',compact('ketqua','a'));
-                }
-            }
-         else
-            {
-                $ketqua="nhập U ";
-                return view('vatly.cuongdodongdien',compact('ketqua'));
-            }
+        $request->validate([
+            'a'=>'required|numeric|min:0.00000000000000000000001',
+            'b'=>'required|numeric|min:0.00000000000000000000001',
+        ],[
+            'a.required'=>'hiệu điện thế bắt buộc phải nhập',
+            'a.numeric'=>'hiệu điện thế buộc phải là số',
+            'a.min'=>'hiệu điện thế phải lớn hơn 0.00000000000000000000001',
+            'b.required'=>'điện trở bắt buộc phải nhập',
+            'b.numeric'=>'điện trở bắt buộc phải là số',
+            'b.min'=>'điện trở phải lớn hơn 0.00000000000000000000001',
+        ]);
+        $a=$_POST['a'];
+        $b=$_POST['b'];
+        //tính kết quả
+        (float)$ketqua=$a/$b;
+                    
+        //xét kết quả là số vô hạn
+        if(is_infinite($ketqua)){
+            $ketqua="kết quả vượt qua giới hạn tính";
+            return view('vatly.cuongdodongdien',compact('ketqua','a','b'));
+        }
+        else{
+            return view('vatly.cuongdodongdien',compact('ketqua','a','b'));
+         }
     }
 
 
