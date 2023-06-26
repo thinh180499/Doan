7b973b61d9a681697a15c9bef742a2bf4509d762
thinh180499 <thinh180499@gmail.com>
2023-06-26 08:42:38 +0700

    updata duongcaotamgiac 2

--- resources/views/toan/duongcaotamgiac.blade.php
+++ resources/views/toan/duongcaotamgiac.blade.php
@@ -1,28 +1,102 @@
 @extends('layouts.layout')
 
 @section('content')
-    <form action="duongcaotamgiac" method="post">
-    <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
-        <div>Tính đường cao của một tam giác h=2*&#40;&#8730&#40;p*&#40;p-a;&#41;*&#40;p-b&#41;*&#40;p-c&#41;&#41;/a:&#40;trong đó:p=&#40;a+b+c&#41;/2&#41;</div>
-        <table>
-            <tr>
-                <td>cạnh a</td>
-                <td><input type="text" name="a" value="<?php if(isset($a)){ if(is_numeric($a)){echo $a;}else{false;} }else{false;} ?>" /></td>
-            </tr>
-            <tr>
-                <td>cạnh b</td>
-                <td><input type="text" name="b" value="<?php if(isset($b)){ if(is_numeric($b)){echo $b;}else{false;} }else{false;} ?>" /></td>
-            </tr>
-            <tr>
-                <td>cạnh c</td>
-                <td><input type="text" name="c" value="<?php if(isset($c)){ if(is_numeric($c)){echo $c;}else{false;} }else{false;} ?>" /></td>
-            </tr>
-            <tr>
-            <td></td>
-            <td><button type="submit">kết quả</button></td>
-            </tr>
-            
-            </table>
-                <?php if(isset($ketqua)){echo $ketqua;}else{false;} ?>
-    </form>
+    <div class="row mt-5">
+        <div class="col">
+            <div class="card-style cardform">
+                <h2>Tính đường cao của một tam giác :</h2>
+                @if ($errors->any())
+                <h2 style="color: red;">
+                    vui lòng kiểm tra lại dữ liệu
+                </h2>
+                 @endif
+        
+                <form action="duongcaotamgiac" method="post">
+                    <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+
+                    <div class="container-fluid mt-4 mb-4">
+                        <div class="row justify-content-center">
+                            <div class="col-auto border rounded-lg p-3 d-flex align-items-center">
+                                <div class="mr-5">
+                                    <span>h=2*</span>
+                                </div>
+                                <div>
+                                    <span>&#8730&#40;p*&#40;p-a;&#41;*&#40;p-b&#41;*&#40;p-c&#41;&#41;</span>
+                                    <hr>
+                                    <span>a</span>
+                                </div>
+                                <div>
+                                    <span>&#40; trong đó:p=</span>
+                                </div>
+                                <div>
+                                    <span>a+b+c</span>
+                                    <hr>
+                                    <span>2</span>
+                                </div>
+                                <div>
+                                    <span>&#41;</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="container">
+                        <div class="row d-flex flex-column">
+                            <div class="col mb-4">
+                                <label class="lb" for="somolchattan">cạnh a</label>
+                                <input type="number" id="somolchattan" name="a" placeholder="Nhập cạnh a"
+                                    class="input" step="any" value="{{ isset($a)&&is_numeric($a) ? $a:old('a')}}" />
+                                    @error('a')
+                                        <span style="color: red;">{{$message}}</span>
+                                    @enderror
+                            </div>
+
+                            <div class="col mb-4">
+                                <label class="lb" for="thetichdungdich">cạnh b</label>
+                                <input type="number" id="thetichdungdich" name="b" placeholder="Nhập cạnh b"
+                                    class="input" step="any" value="{{ isset($b)&&is_numeric($b) ? $b:old('b') }}" />
+                                @error('b')
+                                    <span style="color: red;">{{$message}}</span>
+                                @enderror
+                            </div>
+
+                            <div class="col mb-4">
+                                <label class="lb" for="thetichdungdich">cạnh c</label>
+                                <input type="number" id="thetichdungdich" name="c" placeholder="Nhập cạnh c"
+                                    class="input" step="any" value="{{ isset($c)&&is_numeric($c) ? $c:old('c') }}" />
+                                @error('c')
+                                    <span style="color: red;">{{$message}}</span>
+                                @enderror
+                            </div>
+
+
+                            <div class="col d-flex align-items-center mb-4">
+                                <button class="btn btn-primary me-5 py-0 px-4 calculate" type="submit">=</button>
+                                <span>
+                                {!! isset($ketqua)?$ketqua:false !!}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
+
+@section('css')
+    <style>
+        .container {
+            max-width: 500px;
+        }
+
+        hr {
+            border: none;
+            height: 2px;
+            background: rgb(137, 137, 137);
+        }
+    </style>
+@endsection
+
