dbf4659582f666580beee610d474e97709747936
duyngo022 <duylappter22@gmail.com>
2023-06-28 09:02:28 +0700

    update giao dien luythuacuamotphanso, luythuamunguyenduong

--- resources/views/toan/luythuacuamotphanso.blade.php
+++ resources/views/toan/luythuacuamotphanso.blade.php
@@ -1,85 +1,114 @@
 @extends('layouts.layout')
 
+@section('title', 'Tính lũy thừa của một phân số:')
+
 @section('content')
-    <div class="row mt-5">
-        <div class="col">
-            <div class="card-style cardform">
-                <h2>Tính lũy thừa của một phân số:</h2>
-                @if ($errors->any())
-                <h2 style="color: red;">
-                    vui lòng kiểm tra lại dữ liệu
-                </h2>
-                 @endif
-        
+    <div class="row mt-4">
+        <div class="col-lg-7 tinhtoan">
+            <div class="card-style cardform h-100">
+                <h2>Tính lũy thừa của một phân số::</h2>
+
                 <form action="luythuacuamotphanso" method="post">
                     <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
 
                     <div class="container-fluid mt-4 mb-4">
                         <div class="row justify-content-center">
-                             
-                            
-                            
+
+
+
                             <div class="col-auto border rounded-lg p-3 d-flex align-items-center">
-                            <span>&#40;</span>
+                                <span>&#40;</span>
                                 <div class="mr-5">
-                                   
-                                        <span>a</span>
-                                        <hr>
-                                        <span>b</span>
-                                    
+
+                                    <span>a</span>
+                                    <hr>
+                                    <span>b</span>
+
                                 </div>
-                                <span>&#41;^n</span>
+                                <span>&#41;<sup>n</sup></span>
                             </div>
-                            
+
                         </div>
                     </div>
 
 
                     <div class="container">
                         <div class="row d-flex flex-column">
-                            <div class="col mb-4">
-                                <label class="lb" for="somolchattan"> a</label>
-                                <input type="number" id="somolchattan" name="a" placeholder="Nhập a"
-                                    class="input" step="any" value="{{ isset($a)&&is_numeric($a) ? $a:old('a')}}" />
-                                    @error('a')
-                                        <span style="color: red;">{{$message}}</span>
-                                    @enderror
+                            <div class="col">
+                                <div class="input-style-1">
+                                    <label for="somolchattan"> a</label>
+                                    <input type="number" id="somolchattan" name="a" placeholder="Nhập a"
+                                        class="input" step="any"
+                                        value="{{ isset($a) && is_numeric($a) ? $a : old('a') }}" />
+                                </div>
+                            </div>
+
+                            <div class="col">
+                                <div class="input-style-1">
+                                    <label for="thetichdungdich">b</label>
+                                    <input type="number" id="thetichdungdich" name="b" placeholder="Nhập b"
+                                        class="input" step="any"
+                                        value="{{ isset($b) && is_numeric($b) ? $b : old('b') }}" />
+                                </div>
                             </div>
 
                             <div class="col mb-4">
-                                <label class="lb" for="thetichdungdich">b</label>
-                                <input type="number" id="thetichdungdich" name="b" placeholder="Nhập b"
-                                    class="input" step="any" value="{{ isset($b)&&is_numeric($b) ? $b:old('b') }}" />
-                                @error('b')
-                                    <span style="color: red;">{{$message}}</span>
-                                @enderror
-                                @if (isset($err))
-                                    <h2 style="color: red;">
-                                        {{$err}}
-                                    </h2>
-                                @endif
-                            </div>
-
-                            <div class="col mb-4">
-                                <label class="lb" for="thetichdungdich">n</label>
-                                <input type="number" id="thetichdungdich" name="c" placeholder="Nhập n"
-                                    class="input" step="any" value="{{ isset($c)&&is_numeric($c) ? $c:old('c') }}" />
-                                @error('c')
-                                    <span style="color: red;">{{$message}}</span>
-                                @enderror
+                                <div class="input-style-1">
+                                    <label for="thetichdungdich">n</label>
+                                    <input type="number" id="thetichdungdich" name="c" placeholder="Nhập n"
+                                        class="input" step="any"
+                                        value="{{ isset($c) && is_numeric($c) ? $c : old('c') }}" />
+                                </div>
                             </div>
 
 
                             <div class="col d-flex align-items-center mb-4">
                                 <button class="btn btn-primary me-5 py-0 px-4 calculate" type="submit">=</button>
                                 <span>
-                                {!! isset($ketqua)?$ketqua:false !!}
+                                    {!! isset($ketqua) ? $ketqua : false !!}
                                 </span>
+                                @if ($errors->any())
+                                    <div class="alert-box danger-alert m-0 w-100">
+                                        <div class="alert">
+                                            <h4 class="alert-heading">
+                                                Vui lòng kiểm tra lại dữ liệu
+                                            </h4>
+
+                                            @error('a')
+                                                <p class="text-medium">{{ $message }}</p>
+                                            @enderror
+                                            @error('b')
+                                                <p class="text-medium">{{ $message }}</p>
+                                            @enderror
+                                            @error('c')
+                                                <p class="text-medium">{{ $message }}</p>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                @endif
                             </div>
                         </div>
                     </div>
 
                 </form>
+            </div>
+        </div>
+        <div class="col-lg-5 lythuyet">
+            <div class="card-style cardform h-100">
+                <div class="mb-50">
+                    <h3 class="mb-20">Trong đó</h3>
+                    <ul>
+                        <li>n: số mol (mol)</li>
+                        <li>m: khối lượng chất (gam)</li>
+                        <li>M: khối lượng Mol (gam/mol)</li>
+                    </ul>
+                </div>
+                <div class="mb-30">
+                    <h2 class="mb-30">Lý thuyết</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In ratione vel, eveniet sunt inventore nemo
+                        debitis deleniti quisquam aliquid tenetur, earum maxime vitae id molestiae expedita quasi dolorum!
+                        Consequatur, provident?</p>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/toan/luythuamunguyenduong.blade.php
+++ resources/views/toan/luythuamunguyenduong.blade.php
@@ -1,28 +1,23 @@
-
-
 @extends('layouts.layout')
 
+@section('title', 'Tính lũy thừa với số mũ n')
+
 @section('content')
-    <div class="row mt-5">
-        <div class="col">
-            <div class="card-style cardform">
+    <div class="row mt-4">
+        <div class="col-lg-7 tinhtoan">
+            <div class="card-style cardform h-100">
                 <h2>Tính lũy thừa với số mũ n:</h2>
-                @if ($errors->any())
-                <h2 style="color: red;">
-                    vui lòng kiểm tra lại dữ liệu
-                </h2>
-                 @endif
-        
+
                 <form action="luythuamunguyenduong" method="post">
                     <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
 
                     <div class="container-fluid mt-4 mb-4">
                         <div class="row justify-content-center">
                             <div class="col-auto border rounded-lg p-3 d-flex align-items-center">
-                                <div class="mr-5">
-                                    <span>a^n </span>
+                                <div class="me-3">
+                                    <span>a<sup>n</sup></span>
                                 </div>
-                                
+
                             </div>
                         </div>
                     </div>
@@ -30,34 +25,68 @@
 
                     <div class="container">
                         <div class="row d-flex flex-column">
-                            <div class="col mb-4">
-                                <label class="lb" for="somolchattan">a</label>
-                                <input type="number" id="somolchattan" name="a" placeholder="Nhập a"
-                                    class="input" step="any" value="{{ isset($a)&&is_numeric($a) ? $a:old('a')}}" />
-                                    @error('a')
-                                        <span style="color: red;">{{$message}}</span>
-                                    @enderror
+                            <div class="col">
+                                <div class="input-style-1">
+                                    <label for="somolchattan">a</label>
+                                    <input type="number" id="somolchattan" name="a" placeholder="Nhập a"
+                                        class="input" step="any"
+                                        value="{{ isset($a) && is_numeric($a) ? $a : old('a') }}" />
+                                </div>
                             </div>
 
                             <div class="col mb-4">
-                                <label class="lb" for="thetichdungdich">n</label>
-                                <input type="number" id="thetichdungdich" name="b" placeholder="Nhập n"
-                                    class="input" step="any" value="{{ isset($b)&&is_numeric($b) ? $b:old('b') }}" />
-                                @error('b')
-                                    <span style="color: red;">{{$message}}</span>
-                                @enderror
+                                <div class="input-style-1">
+                                    <label for="thetichdungdich">n</label>
+                                    <input type="number" id="thetichdungdich" name="b" placeholder="Nhập n"
+                                        class="input" step="any"
+                                        value="{{ isset($b) && is_numeric($b) ? $b : old('b') }}" />
+                                </div>
                             </div>
 
                             <div class="col d-flex align-items-center mb-4">
                                 <button class="btn btn-primary me-5 py-0 px-4 calculate" type="submit">=</button>
                                 <span>
-                                {!! isset($ketqua)?$ketqua:false !!}
+                                    {!! isset($ketqua) ? $ketqua : false !!}
                                 </span>
+                                @if ($errors->any())
+                                    <div class="alert-box danger-alert m-0 w-100">
+                                        <div class="alert">
+                                            <h4 class="alert-heading">
+                                                Vui lòng kiểm tra lại dữ liệu
+                                            </h4>
+
+                                            @error('a')
+                                                <p class="text-medium">{{ $message }}</p>
+                                            @enderror
+                                            @error('b')
+                                                <p class="text-medium">{{ $message }}</p>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                @endif
                             </div>
                         </div>
                     </div>
 
                 </form>
+            </div>
+        </div>
+        <div class="col-lg-5 lythuyet">
+            <div class="card-style cardform h-100">
+                <div class="mb-50">
+                    <h3 class="mb-20">Trong đó</h3>
+                    <ul>
+                        <li>n: số mol (mol)</li>
+                        <li>m: khối lượng chất (gam)</li>
+                        <li>M: khối lượng Mol (gam/mol)</li>
+                    </ul>
+                </div>
+                <div class="mb-30">
+                    <h2 class="mb-30">Lý thuyết</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In ratione vel, eveniet sunt inventore nemo
+                        debitis deleniti quisquam aliquid tenetur, earum maxime vitae id molestiae expedita quasi dolorum!
+                        Consequatur, provident?</p>
+                </div>
             </div>
         </div>
     </div>
